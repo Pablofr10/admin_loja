ea075a49cd452bb2b1fe8e31d96861764e1211a0
Pablo Martinez <rafanet.live@gmail.com>
2019-08-25 19:49:55 -0300

    Login opções

--- lib/blocs/login_bloc.dart
+++ lib/blocs/login_bloc.dart
@@ -1,14 +1,22 @@
+import 'dart:async';
+
 import 'package:admin_loja/validators/login_validators.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:rxdart/rxdart.dart';
 
+
+enum LoginState {IDLE, LOADING, SUCCESS, FAIL}
 class LoginBloc extends BlocBase with LoginValidators{
 
   final _emailController = BehaviorSubject<String>();
   final _senhaController = BehaviorSubject<String>();
+  final _stateController = BehaviorSubject<LoginState>();
 
   Stream<String> get outEmail => _emailController.stream.transform(validateEmail);
   Stream<String> get outSenha => _senhaController.stream.transform(validateSenha);
+  Stream<LoginState> get outState => _stateController.stream;
 
   Stream<bool> get outSubmitValid => Observable.combineLatest2(
     outEmail, outSenha, (a, b) => true
@@ -17,12 +25,57 @@ class LoginBloc extends BlocBase with LoginValidators{
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changeSenha => _senhaController.sink.add;
 
+  StreamSubscription _streamSubscription;
 
+  LoginBloc(){
+    _streamSubscription = FirebaseAuth.instance.onAuthStateChanged.listen((user)async{
+      if (user != null) {
+        if(await verifyPrivileges(user)){
+          _stateController.add(LoginState.SUCCESS);
+        }else{
+          _stateController.add(LoginState.FAIL);
+        }
+      } else {
+        FirebaseAuth.instance.signOut();
+        _stateController.add(LoginState.IDLE);
+      }
+    });
+  }
+
+ Future<bool> verifyPrivileges(FirebaseUser user) async{
+    return await Firestore.instance.collection("admins").document(user.uid).get().then((doc){
+      if(doc.data != null){
+        return true;
+      }else{
+        return false;
+      }
+    }).catchError((e){
+      return false;
+    });
+  }
+
+
+  void submit(){
+    final email = _emailController.value;
+    final senha = _senhaController.value;
+
+    _stateController.add(LoginState.LOADING);
+
+    FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: email,
+      password: senha,
+    ).catchError((e){
+      _stateController.add(LoginState.FAIL);
+    });
+
+  }
 
   @override
   void dispose() {
     _emailController.close();
     _senhaController.close();
+    _stateController.close();
+    _streamSubscription.cancel();
   }
 
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -16,55 +16,73 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.grey[800],
-        body: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            Container(),
-            SingleChildScrollView(
-              child: Container(
-                margin: EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Icon(Icons.store_mall_directory,
-                        color: Colors.pinkAccent, size: 160),
-                    InputField(
-                      hint: 'Usuário',
-                      icon: Icons.person,
-                      obscure: false,
-                      stream: _loginBloc.outEmail,
-                      onChanged: _loginBloc.changeEmail,
+        body: StreamBuilder<LoginState>(
+          stream: _loginBloc.outState,
+          initialData: LoginState.SUCCESS,
+          builder: (context, snapshot){
+            print(snapshot.data);
+            switch (snapshot.data){
+              case LoginState.LOADING:
+                return Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(Colors.pinkAccent),
+                  ),
+                );
+              case LoginState.FAIL:
+              case LoginState.SUCCESS:
+              case LoginState.IDLE:
+              return Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                Container(),
+                SingleChildScrollView(
+                  child: Container(
+                    margin: EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(Icons.store_mall_directory,
+                            color: Colors.pinkAccent, size: 160),
+                        InputField(
+                          hint: 'Usuário',
+                          icon: Icons.person,
+                          obscure: false,
+                          stream: _loginBloc.outEmail,
+                          onChanged: _loginBloc.changeEmail,
+                        ),
+                        InputField(
+                          hint: 'Senha',
+                          icon: Icons.vpn_key,
+                          obscure: true,
+                          stream: _loginBloc.outSenha,
+                          onChanged: _loginBloc.changeSenha,
+                        ),
+                        SizedBox(
+                          height: 32,
+                        ),
+                        SizedBox(
+                          height: 50,
+                          child: StreamBuilder<bool>(
+                            stream: _loginBloc.outSubmitValid,
+                            builder: (context, snapshot) {
+                              return RaisedButton(
+                                color: Colors.pinkAccent,
+                                child: Text('Entrar'),
+                                onPressed: snapshot.hasData? (){} : null,
+                                textColor: Colors.white,
+                                disabledColor: Colors.pinkAccent.withAlpha(140),
+                              );
+                            }
+                          ),
+                        )
+                      ],
                     ),
-                    InputField(
-                      hint: 'Senha',
-                      icon: Icons.vpn_key,
-                      obscure: true,
-                      stream: _loginBloc.outSenha,
-                      onChanged: _loginBloc.changeSenha,
-                    ),
-                    SizedBox(
-                      height: 32,
-                    ),
-                    SizedBox(
-                      height: 50,
-                      child: StreamBuilder<bool>(
-                        stream: _loginBloc.outSubmitValid,
-                        builder: (context, snapshot) {
-                          return RaisedButton(
-                            color: Colors.pinkAccent,
-                            child: Text('Entrar'),
-                            onPressed: snapshot.hasData? (){} : null,
-                            textColor: Colors.white,
-                            disabledColor: Colors.pinkAccent.withAlpha(140),
-                          );
-                        }
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
+                  ),
+                )
+              ],
+            );
+            }            
+          }
         ));
   }
 }
